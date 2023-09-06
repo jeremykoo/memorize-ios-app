8327078c7d1a0abe5ae95d33e31ec21ccc3d98f6
Jeremy Koo <skykoo2000@gmail.com>
2023-09-05 21:03:35 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jeremy Koo on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
